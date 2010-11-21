2bd0351b3706316b22830709bf79d873fbbbca49
Tarsis Figueredo Azevedo <tarsis.azevedo@gmail.com>
2010-11-21 14:25:17 -0200

    Change lxml_dependencies script to give option to install the dependencies.

--- client/lxml_dependencies.sh
+++ client/lxml_dependencies.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-#dependencias do libxml
-apt-get install libxml2 libxslt1.1 libxslt-dev
+
+function install_lxml_dependencies () 
+{
+    echo "----------------------------------------------";
+    sudo apt-get install libxml2 libxslt1.1 libxslt-dev;
+    echo "----------------------------------------------";
+}
+
+if [ $USER = "root"]
+then
+    install_lxml_dependencies;
+else
+
+    echo "To install lxml dependencies you should be root user. If you have this depencies installed, skip this step pressing n";
+    echo "Do you want install it? [y/n]";
+    read option;
+    case $option in
+        y) install_lxml_dependencies;;
+        n) echo "--------------------------------------------";;
+    esac;
+fi
+
