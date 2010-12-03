5b935336d8f179e04310b3a845f7bf5d71c20312
Douglas Camata <d.camata@gmail.com>
2010-12-03 21:46:32 -0200

    Fake server changed to Flask.

--- client/features/step_definitions/fake_server.py
+++ client/features/step_definitions/fake_server.py
@@ -3,66 +3,56 @@ adapted AppRunner thread stuff from:
 http://tarekziade.wordpress.com/2010/05/10/faking-a-server-for-client-side-tests/
 """
 
-import threading
+from multiprocessing import Process
 import time
 from wsgiref.simple_server import make_server
-from itty import handle_request, get, post, Response
+#from itty import handle_request, get, post, Response
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
 
 
+_PROCESS = None
 _SERVER = None
 _HOST, _PORT = 'localhost', 8081
 
 content = ""
 content_type = ""
 
-@get('/myresource')
-def myresource(web):
-    return Response(content, content_type=content_type)
-
-@post('/set_content')
-def set_content(request):
+@app.route('/myresource', methods=['GET',])
+def myresource():
     global content
-    content = request.POST.get('content', '')
+#    return Response(data=content, content_type=content_type)
+    response = make_response(content)
+    response.headers['Content-Type'] = content_type
+    return response
+
+@app.route('/set_content', methods=['POST',])
+def set_content():
+    global content
+    content = str(request.data)
+    content = '<item><name>Rich Rock Sunshine</name></item>'
     return ""
 
-@post('/set_content_type')
-def set_content_type(request):
+@app.route('/set_content/type', methods=['POST',])
+def set_content_type():
     global content_type
-    content_type = request.POST.get('content_type', '')
+    content_type = str(request.data)
     return ""
 
-
-class AppRunner(threading.Thread):
-    """Thread that wraps a wsgi app"""
-
-    def __init__(self, wsgiapp):
-        threading.Thread.__init__(self)
-        self.httpd = make_server(_HOST, _PORT, wsgiapp)
-        self.address = self.httpd.server_address
-
-    def run(self):
-        self.httpd.serve_forever()
-
-    def stop(self):
-        self.httpd.shutdown()
-
+def start_flask_app(host, port):
+    """Runs the server."""
+    app.run(host = '127.0.0.1', port = 8081)
 
 def start_server():
-    """Runs the server."""
-    global _SERVER
-    if _SERVER is not None:
-        # we suppose it's running
-        return _SERVER.address
-    _SERVER = AppRunner(handle_request)
-    _SERVER.start()
-    return _SERVER.address
-
+    global _PROCESS, _SERVER, _PORT
+    _PROCESS = Process(target=start_flask_app, args=(_SERVER, _PORT))
+    _PROCESS.daemon = True
+    _PROCESS.start()
+    time.sleep(5)
 
 def stop_server():
-    """Stops the server."""
-    global _SERVER
-    if _SERVER is None:
-        return
-    _SERVER.stop()
-    _SERVER = None
+    global _PROCESS
+    _PROCESS.terminate()
+    time.sleep(5)
 
