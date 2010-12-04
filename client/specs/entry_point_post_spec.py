ce7dd105e565a54f4a47d7593cdebabcb5223dfe
Douglas Camata <d.camata@gmail.com>
2010-12-04 18:47:59 -0200

    spec for post marshall with json

--- client/specs/entry_point_post_spec.py
+++ client/specs/entry_point_post_spec.py
@@ -2,6 +2,7 @@
 
 import unittest
 import urllib2
+from simplejson import dumps
 from ludibrio import Mock, Stub, Dummy
 from should_dsl import should
 from restfulie import Restfulie
@@ -21,4 +22,18 @@ class EntryPointPostSpec(unittest.TestCase):
         Restfulie.at(uri).as_('application/xml').post(content)
         urlencode.validate()
         urlopen.validate()
+
+    def it_should_allow_posting_as_json(self):
+        uri = 'http://coolresource/post-here'
+        content = {"just":"testing"}
+        encoded_content = Dummy()
+        with Mock() as dumps:
+            from json import dumps
+            dumps(content) >> encoded_content
+        with Mock() as urlopen:
+            from urllib2 import urlopen
+            urlopen(uri, encoded_content)
+        Restfulie.at(uri).as_('application/json').post(content)
+        urlopen.validate()
+        dumps.validate()
 
