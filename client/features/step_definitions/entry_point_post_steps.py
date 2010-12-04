dc032114751b8de7ea65131e01e15129a756c9dc
Douglas Camata <d.camata@gmail.com>
2010-12-04 18:23:38 -0200

    steps to post marshalling with json added.

--- client/features/step_definitions/entry_point_post_steps.py
+++ client/features/step_definitions/entry_point_post_steps.py
@@ -1,3 +1,4 @@
+from urllib import urlencode, urlopen
 from should_dsl import should
 from lettuce import *
 from restfulie import Restfulie
@@ -13,4 +14,8 @@ def when_i_post_the_following_content_as(step, content, content_type):
 @step(r'And I request the resource at "(.*)" as raw')
 def then_i_request_the_resource_as_raw(step, uri):
     world.resource = Restfulie.at(uri).raw().get()
+
+@step(u'And I request the resource at "(.*)" as json')
+def and_i_request_the_resource_at_group1_as_json(step, uri):
+    world.resource = Restfulie.at(uri).as_('application/json').get()
 
