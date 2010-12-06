0d3b2a55aa7cc34f87840735400de8be3b5a8f28
Douglas Camata <d.camata@gmail.com>
2010-12-06 01:29:44 -0200

    new steps implemented.

--- client/features/step_definitions/entry_point_post_steps.py
+++ client/features/step_definitions/entry_point_post_steps.py
@@ -1,4 +1,5 @@
 from urllib import urlencode, urlopen
+from json import loads
 from should_dsl import should
 from lettuce import *
 from restfulie import Restfulie
@@ -14,4 +15,14 @@ def when_i_post_the_following_content_as(step, content, content_type):
 @step(r'And I request the resource at "(.*)" as raw')
 def then_i_request_the_resource_as_raw(step, uri):
     world.resource = Restfulie.at(uri).raw().get()
+
+@step(r'When I post a dict containing "(.*)" as "(.*)"')
+def when_i_post_a_dict_containing_group1_as_group2(step, content, content_type):
+    Restfulie.at(world.uri).as_(content_type).post(eval(content))
+
+@step(r'Then the response body is "(.*)" as json')
+def then_the_response_body_is_group1_as_group2(step, response_body):
+    actual = loads(world.resource.response.body)
+    expected = eval(response_body)
+    actual |should| equal_to(expected)
 
