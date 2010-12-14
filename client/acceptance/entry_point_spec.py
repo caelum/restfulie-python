8cd917ed2f3cabf742f19163e4906fdb2e191336
Rodrigo Manhães <rmanhaes@gmail.com>
2010-12-14 15:30:14 -0200

    acceptance spec for xml retrieval converted to unittest

--- client/acceptance/entry_point_spec.py
+++ client/acceptance/entry_point_spec.py
@@ -15,6 +15,28 @@ class EntryPoint(unittest.TestCase):
         resource.response.code |should| be(200)
         resource.response.body |should| equal_to(content)
 
+    def it_retrieves_content_as_xml(self):
+        uri = 'http://localhost:8081/myresource'
+        content = '''<items>
+                        <item>
+                            <name>product</name>
+                            <price>2</price>
+                        </item>
+                        <item>
+                            <name>another product</name>
+                            <price>5</price>
+                        </item>
+                    </items>'''
+
+        for content_type in ('application/xml', 'text/xml'):
+            self.server_content(content, content_type)
+
+            resource = Restfulie.at(uri).get()
+            resource.items[0].name |should| equal_to('product')
+            resource.items[0].price |should| be(2)
+            resource.items[1].name |should| equal_to('another product')
+            resource.items[1].price |should| be(5)
+
     def setUp(self):
         start_server()
 
